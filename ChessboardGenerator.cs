6a16c09de233f1947d4bfbca620ff31ec719e15b
indyhan <indyhan@naver.com>
2025-12-30 08:36:44 +0900

    2025.12.30 First Commit: See the ReadMe

---
+++ ChessboardGenerator.cs
@@ -0,0 +1,99 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ChessboardGenApp
+{
+    public class ChessboardGenerator
+    {
+        public int SquaresX { get; set; } = 10;     // 가로 격자 수
+        public int SquaresY { get; set; } = 7;      // 세로 격자 수
+        public int SquareSize { get; set; } = 80;   // 격자 크기 (Pixel)
+        public int Margin { get; set; } = 100;      // 여백 (Pixel)
+        public double DistortionStrength { get; set; } = 0.2; // 왜곡 강도 (0.0 ~ 0.5)
+
+        public List<string> GenerateImages(string outputDir, int count)
+        {
+            if (!Directory.Exists(outputDir))
+            {
+                Directory.CreateDirectory(outputDir);
+            }
+
+            List<string> generatedFiles = new List<string>();
+
+            // 1. 베이스 체스보드 그리기 (흰 배경)
+            int boardW = SquaresX * SquareSize;
+            int boardH = SquaresY * SquareSize;
+            int imgW = boardW + Margin * 2;
+            int imgH = boardH + Margin * 2;
+
+            Size imgSize = new Size(imgW, imgH);
+
+            using (Mat baseMat = new Mat(imgSize, MatType.CV_8UC1, Scalar.All(255)))
+            {
+                // 검은색 사각형 그리기
+                for (int y = 0; y < SquaresY; y++)
+                {
+                    for (int x = 0; x < SquaresX; x++)
+                    {
+                        if ((x + y) % 2 == 1) // 체스 패턴 조건
+                        {
+                            int px = Margin + x * SquareSize;
+                            int py = Margin + y * SquareSize;
+                            Cv2.Rectangle(baseMat, new Rect(px, py, SquareSize, SquareSize), Scalar.All(0), -1);
+                        }
+                    }
+                }
+
+                Random rand = new Random();
+
+                // 2. 랜덤 변환 적용하여 저장
+                for (int i = 0; i < count; i++)
+                {
+                    // 원본 네 모서리 좌표
+                    Point2f[] srcPts = new Point2f[]
+                    {
+                        new Point2f(0, 0),
+                        new Point2f(imgW, 0),
+                        new Point2f(imgW, imgH),
+                        new Point2f(0, imgH)
+                    };
+
+                    // 왜곡 정도 계산 (이미지 크기 비례)
+                    float maxDx = (float)(imgW * DistortionStrength);
+                    float maxDy = (float)(imgH * DistortionStrength);
+
+                    // 목표 네 모서리 좌표 (랜덤 오프셋 적용)
+                    Point2f[] dstPts = new Point2f[]
+                    {
+                        new Point2f(GetRand(rand, maxDx), GetRand(rand, maxDy)),  // 좌상
+                        new Point2f(imgW - GetRand(rand, maxDx), GetRand(rand, maxDy)), // 우상
+                        new Point2f(imgW - GetRand(rand, maxDx), imgH - GetRand(rand, maxDy)), // 우하
+                        new Point2f(GetRand(rand, maxDx), imgH - GetRand(rand, maxDy)) // 좌하
+                    };
+
+                    // 투시 변환 행렬 및 적용
+                    using (Mat M = Cv2.GetPerspectiveTransform(srcPts, dstPts))
+                    using (Mat warped = new Mat())
+                    {
+                        // 빈 공간은 흰색으로 채움
+                        Cv2.WarpPerspective(baseMat, warped, M, imgSize, InterpolationFlags.Linear, BorderTypes.Constant, Scalar.All(255));
+
+                        string fileName = Path.Combine(outputDir, $"chess_gen_{i + 1:D3}.jpg");
+                        warped.SaveImage(fileName);
+                        generatedFiles.Add(fileName);
+                    }
+                }
+            }
+
+            return generatedFiles;
+        }
+
+        private float GetRand(Random r, float max)
+        {
+            // 0 ~ max 사이의 랜덤 값 반환
+            return (float)(r.NextDouble() * max);
+        }
+    }
+}
---
+++ MainViewModel.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.Win32;
+using OpenCvSharp;
+using OpenCvSharp.WpfExtensions;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ChessboardGenApp
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        #region Properties
+        // 보드 설정
+        private int _squaresX = 10;
+        public int SquaresX { get => _squaresX; set { _squaresX = value; OnPropertyChanged(); } }
+
+        private int _squaresY = 7;
+        public int SquaresY { get => _squaresY; set { _squaresY = value; OnPropertyChanged(); } }
+
+        private int _squareSize = 80;
+        public int SquareSize { get => _squareSize; set { _squareSize = value; OnPropertyChanged(); } }
+
+        // 생성 설정
+        private int _genCount = 20;
+        public int GenCount { get => _genCount; set { _genCount = value; OnPropertyChanged(); } }
+
+        private double _distortion = 0.2; // 0.0 ~ 0.5
+        public double Distortion { get => _distortion; set { _distortion = value; OnPropertyChanged(); } }
+
+        private string _outputPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "ChessImages");
+        public string OutputPath { get => _outputPath; set { _outputPath = value; OnPropertyChanged(); } }
+
+        // 상태 및 결과
+        private string _statusMessage = "Ready";
+        public string StatusMessage { get => _statusMessage; set { _statusMessage = value; OnPropertyChanged(); } }
+
+        private bool _isBusy = false;
+        public bool IsBusy { get => _isBusy; set { _isBusy = value; OnPropertyChanged(); } }
+
+        // 생성된 파일 목록 및 뷰어
+        public ObservableCollection<string> FileList { get; set; } = new ObservableCollection<string>();
+
+        private string _selectedFile;
+        public string SelectedFile
+        {
+            get => _selectedFile;
+            set
+            {
+                _selectedFile = value;
+                OnPropertyChanged();
+                if (!string.IsNullOrEmpty(value) && File.Exists(value))
+                {
+                    LoadImage(value);
+                }
+            }
+        }
+
+        private ImageSource _displayImage;
+        public ImageSource DisplayImage { get => _displayImage; set { _displayImage = value; OnPropertyChanged(); } }
+        #endregion
+
+        public ICommand BrowseCommand { get; }
+        public ICommand GenerateCommand { get; }
+
+        public MainViewModel()
+        {
+            BrowseCommand = new RelayCommand(ExecuteBrowse);
+            GenerateCommand = new RelayCommand(ExecuteGenerate);
+        }
+
+        private void ExecuteBrowse(object obj)
+        {
+            // 폴더 선택 다이얼로그 (OpenFileDialog를 이용한 편법 혹은 FolderBrowserDialog 사용)
+            var dialog = new OpenFileDialog();
+            dialog.ValidateNames = false;
+            dialog.CheckFileExists = false;
+            dialog.CheckPathExists = true;
+            dialog.FileName = "Folder Selection";
+
+            if (dialog.ShowDialog() == true)
+            {
+                OutputPath = Path.GetDirectoryName(dialog.FileName);
+            }
+        }
+
+        private async void ExecuteGenerate(object obj)
+        {
+            if (IsBusy) return;
+            IsBusy = true;
+            StatusMessage = "Generating Images...";
+            FileList.Clear();
+
+            try
+            {
+                await Task.Run(() =>
+                {
+                    ChessboardGenerator gen = new ChessboardGenerator
+                    {
+                        SquaresX = this.SquaresX,
+                        SquaresY = this.SquaresY,
+                        SquareSize = this.SquareSize,
+                        Margin = this.SquareSize, // 여백은 사각형 크기만큼 자동 설정
+                        DistortionStrength = this.Distortion
+                    };
+
+                    var files = gen.GenerateImages(OutputPath, GenCount);
+
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        foreach (var f in files) FileList.Add(f);
+                        if (FileList.Count > 0) SelectedFile = FileList[0];
+                    });
+                });
+
+                StatusMessage = $"Completed. {FileList.Count} images generated.";
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = "Error: " + ex.Message;
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        private void LoadImage(string path)
+        {
+            // 파일 락 방지를 위해 메모리 로드
+            var bmp = new BitmapImage();
+            bmp.BeginInit();
+            bmp.CacheOption = BitmapCacheOption.OnLoad;
+            bmp.UriSource = new Uri(path);
+            bmp.EndInit();
+            DisplayImage = bmp;
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+    // 간단한 RelayCommand 구현
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        public RelayCommand(Action<object> execute) => _execute = execute;
+        public bool CanExecute(object parameter) => true;
+        public void Execute(object parameter) => _execute(parameter);
+        public event EventHandler CanExecuteChanged;
+    }
+}
